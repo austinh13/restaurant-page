741eed802f71c5af25b2983e3cad4d6927463551
austinh13 <austinha236@gmail.com>
2025-07-18 21:54:00 -0500

    added to home

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,10 +6,21 @@ export default function createHome(){
     homePage.classList.add("homePage");
 
     const header = document.createElement("h1");
-    header.innerHTML = "Chompies:A Classic Burger Restaurant";
+    header.innerHTML = "Chompies:Bite Loud. Live Juicy.";
+    header.style.color = "rgb(232, 46, 46)";
     header.classList.add("homeTitle");
 
+    const p1 = document.createElement("p");
+    p1.classList.add("paragraph");
+    p1.innerHTML = "At Chompies, we're flipping the script on boring burgers. Our patties are loud, our toppings are wild, and our buns are always toasted just right (because soggy bread is a crime). Whether you're a triple-stack daredevil or a pickle minimalist, there's a burger here with your name on it—probably written in special sauce.";
+
+    const p2 = document.createElement("p");
+    p2.classList.add("paragraph");
+    p2.innerHTML = "We’re not just a place to eat—we’re a place to chomp. Bring your friends, your big appetite, and your love for things that sizzle. Oh, and don’t forget to try our legendary Waffle Fry Avalanche. It might change your life.";
+
     homePage.appendChild(header);
+    homePage.appendChild(p1);
+    homePage.appendChild(p2);
 
     content.append(homePage);
 }
