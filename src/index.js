48f7f376ae63c565f63e82dc5d4cc1f68c479222
austinh13 <austinha236@gmail.com>
2025-07-18 21:28:11 -0500

    tangible home page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import header from "./pages/header.js";
+import home from "./pages/home.js";
 import "./styles/style.css"; // ✅ Import CSS here
 
 
 document.addEventListener("DOMContentLoaded", () => {
   header();
+  home();
 });
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,15 @@
 export default function createHome(){
 
+    const content = document.getElementById("content");
+
     const homePage = document.createElement("div");
     homePage.classList.add("homePage");
 
     const header = document.createElement("h1");
-    header.innerHTML = "Chompies: A Classic Burger Restaurant";
+    header.innerHTML = "Chompies:A Classic Burger Restaurant";
     header.classList.add("homeTitle");
+
+    homePage.appendChild(header);
+
+    content.append(homePage);
 }
