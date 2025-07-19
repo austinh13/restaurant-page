475145a9f51deb68b25a68378d94342a5bbdb5ce
austinh13 <austinha236@gmail.com>
2025-07-18 19:12:05 -0500

    start home page

---
+++ src/pages/home.js
@@ -0,0 +1,9 @@
+export default function createHome(){
+
+    const homePage = document.createElement("div");
+    homePage.classList.add("homePage");
+
+    const header = document.createElement("h1");
+    header.innerHTML = "Chompies: A Classic Burger Restaurant";
+    header.classList.add("homeTitle");
+}
