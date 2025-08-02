f080399311484b08e0500621e5fe1f21e5d643de
austinh13 <austinha236@gmail.com>
2025-08-02 18:08:42 -0500

    modify info

--- src/pages/info.js
+++ src/pages/info.js
@@ -7,9 +7,19 @@ export default function createInfo(){
     const header = document.createElement("h1");
     header.innerHTML = "Chompies: Info";
 
+    const locationHeader = document.createElement("h2");
+    locationHeader.innerHTML = "📍 Location";
+    const locationTitle = document.createElement("p");
+    locationTitle.innerHTML = "Chompies HQ";
+    const locationAddy = document.createElement("p");
+    locationAddy.innerHTML = "404 Burger Blvd. Snackville, ZZ 99999";
+
 
 
     infoDiv.appendChild(header);
+    infoDiv.appendChild(locationHeader);
+    infoDiv.appendChild(locationTitle);
+    infoDiv.appendChild(locationAddy);
 
     content.appendChild(infoDiv);
 }
