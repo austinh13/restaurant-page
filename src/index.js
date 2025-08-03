586fcd6be15fd1b88e31e62ae7e7aa0a613a44cc
austinh13 <austinha236@gmail.com>
2025-08-03 16:46:27 -0500

    info + header phone sizing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import home from "./pages/home.js";
 import menu from "./pages/menu.js";
 import info from "./pages/info.js";
 import "./styles/style.css"; 
-
+import "./styles/headerstyle.css";
+import "./styles/homestyle.css";
+import "./styles/infostyle.css";
+import "./styles/menustyle.css";
 
 
 document.addEventListener("DOMContentLoaded", () => {
